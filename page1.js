e062df73a95e4b7e196f70ce505ee687b9e02c80
D4_Azim_62776 <azim.pathan4297@gmail.com>
2022-06-20 23:51:18 +0530

    isOdd added

--- page1.js
+++ page1.js
@@ -2,9 +2,20 @@ function isEven(number){
     if(number%2==0){
         console.log(`${number} is even`)
     }else{
-        console.log(`${number} is odd`)
+        console.log(`${number} is not even`)
     }
 
 }
 isEven(10)
 isEven(11)
+
+function isOdd(number){
+    if(number%2!=0){
+        console.log(`${number} is odd`)
+    }else{
+        console.log(`${number} is not odd`)
+    }
+
+}
+isOdd(9)
+isOdd(8)
